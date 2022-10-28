f5e95f3cb5a745aeabd3fbe1bc265154e7c8b08a
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2022-10-28 18:56:45 +0300

    delete sql package

--- src/main/java/com/example/leetcodeproblems/Problems/SQL/Joins/leftJoinProblem.sql
+++
@@ -1,7 +0,0 @@
-select a.name as Customers from Customers a
-left join Orders b on a.id = b.customerId
-where customerId is null
-
-#se face joinj de la dreapta la stinga,
-# si unde nu este id, se pune null,
-# apoi afsam toate ce au null
