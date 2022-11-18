d79e7e1243b38679e5bf5c47bd403c297b93f362
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2022-11-18 19:51:15 +0200

    add autoboxing and incapsulare

--- src/main/java/com/example/leetcodeproblems/Triks/Class1.java
+++ src/main/java/com/example/leetcodeproblems/Triks/Class1.java
@@ -1,15 +1,14 @@
 package com.example.leetcodeproblems.Triks;
 
 public class Class1 {
-    private int num0;
-    public int num1;
+    private int num; //schimbam numele
+    public int num2;
 
     public int getNum1() {
         return num1;
-    }
+    } //schimbarile se fac doar in interiorul clasei, dar nu in afara
 
     public void setNum1(int num1) {
         this.num1 = num1;
     }
-
 }
--- src/main/java/com/example/leetcodeproblems/Triks/TEst.java
+++ src/main/java/com/example/leetcodeproblems/Triks/TEst.java
@@ -4,7 +4,7 @@ public class TEst {
     public static void main(String[] args) {
         Class1 class1 = new Class1();
 
-        class1.num1 = 12;
+        class1.num2 = 12;
         class1.setNum1(12);
 
     }
